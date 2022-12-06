06f4e021902ca63150da233eab702f78ffea187a
Rahul Sharma <rahul.sharma@Technovert.com>
2022-12-06 15:24:31 +0530

    website

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -10,9 +10,9 @@ export default class Header extends Component {
             <img src="./Assets/Title.png" alt="title" width={77} />
             <span className='title-heading mt-3'>TITLE</span>
           </div>
-          <div className="settings ps-5 ms-5 ">
+          <div className="settings ps-2 ms-3 ">
             <Icon className='notification-bell ms-5 mt-3 me-2' icon="mingcute:notification-fill" width={21} />
-            <Icon className='setting-icon mt-3 ms-3 me-3 ' icon="material-symbols:settings" width={21} />
+            <Icon className='setting-icon mt-3 mx-4  ' icon="material-symbols:settings" width={21} />
             <span className="person-profile mt-2 pt-1">
               <div className="profile-name m-2 px-3 pe-4 me-3">
                 <p className='john'>John Samuel</p>
